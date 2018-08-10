0a655fae825ce23201bc5e0c383715771bdff75a
richard lucas <richard lucas>
2018-08-10 18:00:31 +0100

    Enabled logged in user forenames to be displayed at the top left. Also started init work on changing the look and feel o

--- client_side/src/app/app.component.ts
+++ client_side/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+        
+    constructor() {
+        document.body.style.background = 'rgb(26, 26, 26)';
+    }
+
   title = 'app';
 }
